b001eecdd55ea5505aeeb5b432f4c014c94865f5
Surendra Shrestha <110609419+shresthasurendra@users.noreply.github.com>
2022-09-01 17:26:35 +0545

    Day 2

--- test.dart
+++ test.dart
@@ -9,23 +9,68 @@ main(){
   // var res = a > 9 ? "value greater than 10" : "value lesser than or equal to 10";
   // print(res);
 
-   outerloop: // This is the label name 
+  //  outerloop: // This is the label name 
    
-   for (var i = 0; i < 5; i++) { 
-      print("Outerloop: ${i}"); 
-      innerloop: 
+  //  for (var i = 0; i < 5; i++) { 
+  //     print("Outerloop: ${i}"); 
+  //     innerloop: 
       
-      for (var j = 0; j < 5; j++) { 
-         if (j > 3 ) break ; 
+      // for (var j = 0; j < 5; j++) { 
+      //    if (j > 3 ) break ; 
          
         //  Quit the innermost loop 
-         if (i == 2) break innerloop; 
+        //  if (i == 2) break innerloop; 
          
       //   //  // Do the same thing 
-         if (i == 4) break outerloop; 
+        //  if (i == 4) break outerloop; 
          
       //    // Quit the outer loop 
-         print("Innerloop: ${j}"); 
-      } 
-   } 
+        //print("Innerloop: ${j}"); 
+//       } 
+//    } 
+
+  // print(num.parse('12'));
+  // print(num.parse('12.21'));
+
+  // multi-line String using triple quotes
+
+  // String par=''' This is the multi-line string.
+  //               Here, we can write multiple lines of sentences in single varaible. ''';
+  //   print(par);
+
+  // String str1 = "A"; 
+  // String str2 = "A"; 
+  // String str3 = "B"; 
+   
+  //  print("str1.compareTo(str2): ${str1.compareTo(str2)}"); 
+  //  print("str1.compareTo(str3): ${str1.compareTo(str3)}"); 
+  //  print("str3.compareTo(str2): ${str3.compareTo(str2)}"); 
+
+  // bool test; 
+  // test = 4 > 5; 
+  // print(test);
+
+  //  var str = 'abc'; 
+  //  if(str) { 
+  //     print('String is not empty'); 
+  //  } else { 
+  //     print('Empty String'); 
+  //  }
+  // var list =[1,2,3];
+
+//var list = [];
+// var list2 = [0, ...list];
+// assert(list2.length == 3);
+// print("No error found");
+
+
+// var list2 = [0, ...?list];
+// assert(list2.length == 1); //right code
+// assert(list2.length ==3); //wrong code
+// print("No error");
+
+
+
+
+
 }
